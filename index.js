eac6d1dd8b02301051d79645363fd476e001853b
Pierre Basck <pierre.basck@gmail.com>
2022-11-02 13:33:45 +0100

    challenge

---
+++ index.js
@@ -0,0 +1,5 @@
+// const firstname = "Pierre";
+// const city = "Lyon";
+// const language = "Javascript";
+require('dotenv').config()
+console.log(`I am ${process.env.MY_NAME}, wilder in ${process.env.MY_CITY}, and I love ${process.env.MY_LANGUAGE}`);
